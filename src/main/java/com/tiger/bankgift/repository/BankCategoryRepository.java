e03cba9248f613d574e4b076f0c0126044e262dd
linyuetiangmcc <13902220411@139.com>
2019-03-06 08:58:01 +0800

    update package

--- src/main/java/com/tiger/bankgift/repository/BankCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tiger.bankgift.repository;
-
-import com.tiger.bankgift.dataobject.BankCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankCategoryRepository extends JpaRepository<BankCategory,Integer>{
-}
